bb2cb125edba78b5b7f4bbb2c8dad3b6faa1a593
Ambar_Fulzele <ambar.fulzele@northgateps.com>
2017-09-21 16:33:12 +0530

    Task 1.3

--- InterviewTest.DriverData/Analysers/GetawayDriverAnalyser.cs
+++ InterviewTest.DriverData/Analysers/GetawayDriverAnalyser.cs
@@ -1,14 +1,77 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace InterviewTest.DriverData.Analysers
 {
 	// BONUS: Why internal?
 	internal class GetawayDriverAnalyser : IAnalyser
 	{
-		public HistoryAnalysis Analyse(IReadOnlyCollection<Period> history)
+        const decimal MINIMUMSPEED = 0, MAXIMUMSPEED = 80, ZEROSPEED = 0;
+        TimeSpan startTime = new TimeSpan(13, 0, 0);
+        TimeSpan endTime = new TimeSpan(14, 0, 0);
+
+        public HistoryAnalysis Analyse(IReadOnlyCollection<Period> history)
 		{
-			throw new NotImplementedException();
-		}
-	}
+            HistoryAnalysis result = new HistoryAnalysis
+            {
+                AnalysedDuration = TimeSpan.Zero,
+                DriverRating = 0
+            };
+
+            List<HistoryAnalysis> histCollection = new List<HistoryAnalysis>();
+
+            //Ignore anything before the first non-zero speed in a day, and after the last
+            history.OrderBy(h => h.Start).SkipWhile(h => h.AverageSpeed == 0).Reverse().SkipWhile(h => h.AverageSpeed == 0).ToList().ForEach(h =>
+            {
+                // Start and End time are in the range
+                if (h.Start.TimeOfDay >= startTime && h.Start.TimeOfDay <= endTime
+                    && h.End.TimeOfDay >= startTime && h.End.TimeOfDay <= endTime)
+                {
+                    // Add valid data to collection for weighted average rating calculation
+                    histCollection.Add(getValidHistoryData(h, h.Start.TimeOfDay, h.End.TimeOfDay));
+                }
+
+                // Start time is out of range and end time is in the range
+                if (h.Start.TimeOfDay < startTime
+                    && h.End.TimeOfDay >= startTime && h.End.TimeOfDay < endTime)
+                {
+                    // Add valid data to collection for weighted average rating calculation
+                    histCollection.Add(getValidHistoryData(h, startTime, h.End.TimeOfDay));
+                }
+
+                // Start time in the range and end time is out of range
+                if (h.Start.TimeOfDay >= startTime && h.Start.TimeOfDay <= endTime
+                    && h.End.TimeOfDay > endTime)
+                {
+                    // Add valid data to collection for weighted average rating calculation
+                    histCollection.Add(getValidHistoryData(h, h.Start.TimeOfDay, endTime));
+                }
+
+                // Start time and end time both are out of range
+                if (h.Start.TimeOfDay < startTime && h.End.TimeOfDay > endTime)
+                {
+                    // Add valid data to collection for weighted average rating calculation
+                    histCollection.Add(getValidHistoryData(h, startTime, endTime));
+                }
+            });
+
+            result = AnalyserHelper.getFinalRating(histCollection);
+
+            return result;
+        }
+
+        private HistoryAnalysis getValidHistoryData(Period p, TimeSpan startingTime, TimeSpan endingTime)
+        {
+            HistoryAnalysis histdata = new HistoryAnalysis();
+
+            histdata.AnalysedDuration = (endingTime - startingTime);
+
+            //Speeds between 0 and 80 map linearly to [0,1] and Speeds above 80 map to 1
+            histdata.DriverRating = ((p.AverageSpeed > MAXIMUMSPEED) ? 1 :
+                                            ((p.AverageSpeed <= MAXIMUMSPEED && p.AverageSpeed > MINIMUMSPEED) ? (p.AverageSpeed / MAXIMUMSPEED) : ZEROSPEED));
+
+            return histdata;
+        }
+    }
 }
