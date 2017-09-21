6076cff114a3f3a5e9061f2c0dd0687599390d4e
Rafiqull Hasan Sheikh <rafiqull.sheikh@northgateps.com>
2017-09-21 13:16:06 +0530

    Task 1.1

---
+++ InterviewTest.DriverData/Analysers/AnalyserHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewTest.DriverData.Analysers
+{
+    public static class AnalyserHelper
+    {
+        public static HistoryAnalysis getFinalRating(ICollection<HistoryAnalysis> histCollection)
+        {
+            try
+            { 
+                HistoryAnalysis histAnalysis = new HistoryAnalysis
+                {
+                    AnalysedDuration = TimeSpan.Zero,
+                    DriverRating = 0
+                };
+
+                if (histCollection.Count > 0)
+                {
+                    histCollection.ToList().ForEach(h =>
+                        {
+                            histAnalysis.AnalysedDuration += h.AnalysedDuration;
+                            histAnalysis.DriverRating += (h.AnalysedDuration.Ticks * h.DriverRating);
+                        }
+                    );
+
+                    histAnalysis.DriverRating = histAnalysis.DriverRating / histAnalysis.AnalysedDuration.Ticks;
+                }
+
+                return histAnalysis;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
--- InterviewTest.DriverData/Analysers/DeliveryDriverAnalyser.cs
+++ InterviewTest.DriverData/Analysers/DeliveryDriverAnalyser.cs
@@ -1,14 +1,76 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace InterviewTest.DriverData.Analysers
 {
 	// BONUS: Why internal?
 	internal class DeliveryDriverAnalyser : IAnalyser
 	{
-		public HistoryAnalysis Analyse(IReadOnlyCollection<Period> history)
+        const decimal MINIMUMSPEED = 0, MAXIMUMSPEED = 30, ZEROSPEED = 0;
+        TimeSpan startTime = new TimeSpan(9, 0, 0);
+        TimeSpan endTime = new TimeSpan(17, 0, 0);
+
+        public HistoryAnalysis Analyse(IReadOnlyCollection<Period> history)
 		{
-			throw new NotImplementedException();
-		}
-	}
+            HistoryAnalysis result = new HistoryAnalysis
+            {
+                AnalysedDuration = TimeSpan.Zero,
+                DriverRating = 0
+            };
+            
+            List<HistoryAnalysis> histCollection = new List<HistoryAnalysis>();
+
+            history.OrderBy(h => h.Start).ToList().ForEach(h =>
+            {
+                // Ignore anything outside of 9-5;
+                // Start and End time are in the range
+                if (h.Start.TimeOfDay >= startTime && h.Start.TimeOfDay <= endTime 
+                    && h.End.TimeOfDay >= startTime && h.End.TimeOfDay <= endTime)
+                {
+                    // Add valid data to collection for weighted average rating calculation
+                    histCollection.Add(getValidHistoryData(h, h.Start.TimeOfDay, h.End.TimeOfDay));
+                }
+
+                // Start time is out of range and end time is in the range
+                if (h.Start.TimeOfDay < startTime
+                    && h.End.TimeOfDay >= startTime && h.End.TimeOfDay < endTime)
+                {
+                    // Add valid data to collection for weighted average rating calculation
+                    histCollection.Add(getValidHistoryData(h, startTime, h.End.TimeOfDay));
+                }
+
+                // Start time in the range and end time is out of range
+                if (h.Start.TimeOfDay >= startTime && h.Start.TimeOfDay <= endTime
+                    && h.End.TimeOfDay > endTime)
+                {
+                    // Add valid data to collection for weighted average rating calculation
+                    histCollection.Add(getValidHistoryData(h, h.Start.TimeOfDay, endTime));
+                }
+
+                // Start time and end time both are out of range
+                if (h.Start.TimeOfDay < startTime && h.End.TimeOfDay > endTime)
+                {
+                    // Add valid data to collection for weighted average rating calculation
+                    histCollection.Add(getValidHistoryData(h, startTime, endTime));
+                }
+            });
+
+            result = AnalyserHelper.getFinalRating(histCollection);
+
+            return result;
+        }
+
+        private HistoryAnalysis getValidHistoryData(Period p, TimeSpan startingTime, TimeSpan endingTime)
+        {
+            HistoryAnalysis histdata = new HistoryAnalysis();
+
+            histdata.AnalysedDuration = (endingTime - startingTime);
+
+            //Speeds between 0 and 30 map linearly to [0,1] and Speeds above 30 map to 0
+            histdata.DriverRating = ((p.AverageSpeed <= MAXIMUMSPEED && p.AverageSpeed > MINIMUMSPEED) ? (p.AverageSpeed / MAXIMUMSPEED) : ZEROSPEED);
+
+            return histdata;
+        }
+    }
 }
