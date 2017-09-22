4eee6e3220c95a8ebfc93f5eb6bd5a78c048b9d2
Ambar_Fulzele <ambar.fulzele@northgateps.com>
2017-09-22 13:59:39 +0530

    Task 2

--- InterviewTest.DriverData/AnalyserLookup.cs
+++ InterviewTest.DriverData/AnalyserLookup.cs
@@ -1,20 +1,62 @@
 using System;
 using InterviewTest.DriverData.Analysers;
+using System.Collections.Generic;
 
 namespace InterviewTest.DriverData
 {
 	public static class AnalyserLookup
 	{
-		public static IAnalyser GetAnalyser(string type)
-		{
-			switch (type)
-			{
-				case "friendly":
-					return new FriendlyAnalyser();
+        public interface ICreateAnalyser
+        {
+            IAnalyser CreateAnalyser();
+        }
 
-				default:
-					throw new ArgumentOutOfRangeException(nameof(type), type, "Unrecognised analyser type");
-			}
-		}
+        public class DeliveryDriver : ICreateAnalyser
+        {
+            public IAnalyser CreateAnalyser()
+            {
+                return new DeliveryDriverAnalyser();
+            }
+        }
+
+        public class FormulaOne : ICreateAnalyser
+        {
+            public IAnalyser CreateAnalyser()
+            {
+                return new FormulaOneAnalyser();
+            }
+        }
+
+        public class Friendly : ICreateAnalyser
+        {
+            public IAnalyser CreateAnalyser()
+            {
+                return new FriendlyAnalyser();
+            }
+        }
+
+        public class GetawayDriver : ICreateAnalyser
+        {
+            public IAnalyser CreateAnalyser()
+            {
+                return new GetawayDriverAnalyser();
+            }
+        }
+
+        private static Dictionary<string, Func<IAnalyser>> analyserType = new Dictionary<string, Func<IAnalyser>>()
+        {
+            {"DeliveryDriver", () => { return new DeliveryDriver().CreateAnalyser(); }},
+            {"FormulaOne", () => { return new FormulaOne().CreateAnalyser(); }},
+            {"Friendly", () => { return new Friendly().CreateAnalyser(); }},
+            {"GetawayDriver", () => {return new GetawayDriver().CreateAnalyser(); } }
+        };
+        
+        public static IAnalyser GetAnalyser(string type)
+		{
+            if (analyserType.ContainsKey(type))
+                return analyserType[type]();
+            else
+                throw new ArgumentOutOfRangeException(nameof(type), type, "Unrecognised analyser type");            
+        }
 	}
 }
